9c286761b424b8322f4da536cc8ddd5f99fa5d8e
Panagiotis Drougas <49459945+LittleDTLe@users.noreply.github.com>
2019-04-29 18:24:07 +0300

    Roll Chances

--- DiceRoller/DiceRoll_Chances.py
+++ DiceRoller/DiceRoll_Chances.py
@@ -19,27 +19,63 @@ def check(user):
         print("Invalid Input!")
 
 def against(user):
-    dice_type = [4, 6, 8, 10, 12, 20 , 100]
     chance = random.random()
-    if chance >= 0.75:
-        check(user)
-    else:
+    chance = random.random()
+    if chance < 0.85:
         sep = user.lower().index('d')
         try:
             quant = int(user[0:sep])
             type = int(user[sep + 1:])
+            if type in (4, 6, 8, 10, 12, 20, 100):
+                # Roll
+                total_roll = 0
+                for roll in range(quant):
+                    total_roll += (random.randrange(type) + 1)
+                if total_roll >= ((quant * type) / 3) * 2:
+                    total_roll -= (type / 2)
+                print("You rolled: " + str(int(total_roll)))
+            else:
+                print("Not valid dice type!")
         except:
             print("Invalid Input!")
-        for dice in range(len(dice_type)):
-            if type == dice_type[dice]:
-                rollDice(quant,dice_type[dice-1])
+    else:
+        check(user)
 
+def favor(user):
+    chance = random.random()
+    if chance > 0.25:
+        #High Roll
+        sep = user.lower().index('d')
+        try:
+            quant = int(user[0:sep])
+            type = int(user[sep + 1:])
+            if type in (4, 6, 8, 10, 12, 20, 100):
+                #Roll
+                total_roll = 0
+                for roll in range(quant):
+                    total_roll += (random.randrange(type) + 1)
+                if total_roll < ((quant*type)/3) * 2:
+                    total_roll += (type / 2)
+                print("You rolled: " + str(int(total_roll)))
+            else:
+                print("Not valid dice type!")
+        except:
+            print("Invalid Input!")
+    else:
+        check(user)
 
 while True:
     print("To exit type [EXIT]")
-    user = input("Enter what you want to roll[e.x: 2d8]: ")
-    if user.upper() == "EXIT":
+    setting = input("Enter chances of roll[e.x: For, Against, Neutral]: ")
+    if setting.upper() == "EXIT":
         exit()
     else:
-        #check(user)
-        against(user)
+        user = input("Enter what you want to roll[e.x: 2d8]: ")
+        if setting.upper() == "FOR":
+            favor(user)
+        elif setting.upper() == "AGAINST":
+            against(user)
+        elif setting.upper() == "NEUTRAL":
+            check(user)
+        else:
+            print("NOT VALID CHANCE TYPE!")
