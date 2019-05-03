a77352736d1a343cdc61cbdf3f49b3092df1f3f4
Panagiotis Drougas <49459945+LittleDTLe@users.noreply.github.com>
2019-05-04 00:08:06 +0300

    Delete Entries

--- Tables/DM_Database.py
+++ Tables/DM_Database.py
@@ -9,6 +9,7 @@ mydb = mysql.connector.connect(
     database="dm_database"
 )
 
+
 # my_cursor.execute("CREATE DATABASE dm_database")
 
 # CREATE LOOT TABLE
@@ -18,12 +19,14 @@ def createLootTable():
                       "id INTEGER(3) AUTO_INCREMENT PRIMARY KEY)")
     print("LOOT TABLE CREATED")
 
+
 # CREATE ESTABLISHMENT TABLE
 def createEstTable():
     my_cursor = mydb.cursor()
     my_cursor.execute("CREATE TABLE establishment (name VARCHAR(30), description MEDIUMTEXT, "
                       "id INTEGER(3) AUTO_INCREMENT PRIMARY KEY)")
     print("ESTABLISHMENT TABLE CREATED")
+
 
 # CREATE ENCOUNTER TABLE
 def createEncTable():
@@ -32,12 +35,14 @@ def createEncTable():
                       "id INTEGER(3) AUTO_INCREMENT PRIMARY KEY)")
     print("ENCOUNTER TABLE CREATED")
 
+
 # Show the available Tables
 def showTables():
     my_cursor = mydb.cursor()
     my_cursor.execute("SHOW TABLES")
     for table in my_cursor:
         print(table[0])
+
 
 # Populate Loot Table
 def popLoot():
@@ -52,14 +57,15 @@ def popLoot():
             records[row].append(col)
             records[row][col] = 0
     for row in range(entries):
-            for col in range(2):
-                #records[row][col] = input("Field: " + str(col+1) + " Entry: " + str(row+1) + " : ")
-                if col == 0:
-                    records[row][col] = input("Enter name: ")
-                else:
-                    records[row][col] = input("Enter description: ")
+        for col in range(2):
+            # records[row][col] = input("Field: " + str(col+1) + " Entry: " + str(row+1) + " : ")
+            if col == 0:
+                records[row][col] = input("Enter name: ")
+            else:
+                records[row][col] = input("Enter description: ")
     my_cursor.executemany(sql, records)
     mydb.commit()
+
 
 # Populate Establishment Table
 def popEst():
@@ -82,6 +88,7 @@ def popEst():
     my_cursor.executemany(sql, records)
     mydb.commit()
 
+
 # Populate Encounter Table
 def popEnc():
     my_cursor = mydb.cursor()
@@ -103,6 +110,7 @@ def popEnc():
     my_cursor.executemany(sql, records)
     mydb.commit()
 
+
 # Random Entry from Loot Table
 def randomLoot():
     my_cursor = mydb.cursor()
@@ -119,6 +127,7 @@ def randomLoot():
     my_cursor.execute(sql, id)
     entry = my_cursor.fetchone()
     print("Name: " + entry[0] + "\nDescription: " + entry[1])
+
 
 # Random Entry from Establishment Table
 def randomEst():
@@ -137,17 +146,38 @@ def randomEst():
     entry = my_cursor.fetchone()
     print("Name: " + entry[0] + "\nDescription: " + entry[1])
 
+
+# Radnom Entry from Encounter Table
+def randomEnc():
+    my_cursor = mydb.cursor()
+    my_cursor.execute("SELECT * FROM encounter")
+    lines = my_cursor.fetchall()
+    entries = 0
+    for line in lines:
+        entries += 1
+    my_cursor.close()
+    ran_entry = random.randint(1, entries)
+    my_cursor = mydb.cursor()
+    sql = "SELECT * FROM encounter WHERE id = %s"
+    id = (ran_entry,)
+    my_cursor.execute(sql, id)
+    entry = my_cursor.fetchone()
+    print("Name: " + entry[0] + "\nDescription: " + entry[1])
+
+
 # DROP Loot
 def dropLoot():
     my_cursor = mydb.cursor()
     my_cursor.execute("DROP TABLE IF EXISTS loot")
     print("Loot Table DROPPED...")
 
+
 # DROP Establishment
 def dropEst():
     my_cursor = mydb.cursor()
     my_cursor.execute("DROP TABLE IF EXISTS establishment")
     print("Establisment Table DROPPED...")
+
 
 # DROP Encounter
 def dropEnc():
@@ -165,8 +195,118 @@ def modTables():
 def tables():
     print("1. Loot\n"
           "2. Establishment\n"
-          "3. Encounter")# Others to be added
+          "3. Encounter")  # Others to be added
 
+# Print Loot Data
+def printLoot():
+    my_cursor = mydb.cursor()
+    my_cursor.execute("SELECT * FROM loot")
+    result = my_cursor.fetchall()
+    for row in result:
+        print("NAME\tDESCRIPTION\t\tID")
+        print(row[0], row[1] + "\t\t", row[2])
+
+
+# Print Establisment Data
+def printEst():
+    my_cursor = mydb.cursor()
+    my_cursor.execute("SELECT * FROM establisment")
+    result = my_cursor.fetchall()
+    for row in result:
+        print("NAME\tDESCRIPTION\t\tID")
+        print(row[0], row[1] + "\t\t", row[2])
+
+
+# Print Encounter Data
+def printEnc():
+    my_cursor = mydb.cursor()
+    my_cursor.execute("SELECT * FROM encounter")
+    result = my_cursor.fetchall()
+    for row in result:
+        print("NAME\tDESCRIPTION\t\tID")
+        print(row[0], row[1] + "\t\t", row[2])
+
+
+# Update Loot Data
+def updateLoot(pos, key):
+    my_cursor = mydb.cursor()
+    if pos.upper() == "NAME":
+        sql = "UPDATE loot SET name = %s WHERE id = %s"
+        id = (key,)
+        name = (input("Enter name: "))
+        my_cursor.execute(sql, name, id)
+        mydb.commit()
+    elif pos.upper() == "DESCRIPTION":
+        sql = "UPDATE loot SET description = %s WHERE id = %s"
+        id = (key,)
+        description = input("Enter description: ")
+        my_cursor.execute(sql, description, id)
+        mydb.commit()
+
+
+# Update Establishment Data
+def updateEst(pos, key):
+    my_cursor = mydb.cursor()
+    if pos.upper() == "NAME":
+        sql = "UPDATE establishment SET name = %s WHERE id = %s"
+        id = (key,)
+        name = (input("Enter name: "))
+        my_cursor.execute(sql, name, id)
+        mydb.commit()
+    elif pos.upper() == "DESCRIPTION":
+        sql = "UPDATE establishment SET description = %s WHERE id = %s"
+        id = (key,)
+        description = input("Enter description: ")
+        my_cursor.execute(sql, description, id)
+        mydb.commit()
+
+
+# Update Encounter Data
+def updateEnc(pos, key):
+    my_cursor = mydb.cursor()
+    if pos.upper() == "NAME":
+        sql = "UPDATE encounter SET name = %s WHERE id = %s"
+        id = (key,)
+        name = (input("Enter name: "))
+        my_cursor.execute(sql, name, id)
+        mydb.commit()
+    elif pos.upper() == "DESCRIPTION":
+        sql = "UPDATE encounter SET description = %s WHERE id = %s"
+        id = (key,)
+        description = input("Enter description: ")
+        my_cursor.execute(sql, description, id)
+        mydb.commit()
+
+# Delete Entry from Loot
+def deleteLootData(key):
+    my_cursor = mydb.cursor()
+    sql = "Delete from loot where id = %s"
+    id = (key,)
+    my_cursor.execute(sql, id)
+    mydb.commit()
+
+# Delete Entry from Establishment
+def deleteEstData(key):
+    my_cursor = mydb.cursor()
+    sql = "Delete from establishment where id = %s"
+    id = (key,)
+    my_cursor.execute(sql, id)
+    mydb.commit()
+
+# Delete Entry from Loot
+def deleteEncData(key):
+    my_cursor = mydb.cursor()
+    sql = "Delete from encounter where id = %s"
+    id = (key,)
+    my_cursor.execute(sql, id)
+    mydb.commit()
+
+# Modification Options
+def modOption():
+    print("1. Modify\n"
+          "2. Delete")
+
+# Program
 while True:
     print("1. SHOW TABLES\n"
           "2. CHOOSE TABLE\n"
@@ -179,15 +319,18 @@ while True:
         showTables()
     # Random Table Value
     elif choice == 2:
+        # After choosing a table, a random entry of the table will show
         showTables()
         table_choice = input("Select Table: ")
         if table_choice.upper() == "LOOT":
             randomLoot()
             os.system('cls')
-        else:
+        elif table_choice.upper() == "ESTABLISHMENT":
             randomEst()
             os.system('cls')
-        # After choosing a table, a random entry of the table will show
+        elif table_choice.upper() == "ENCOUNTER":
+            randomEnc()
+            os.system('cls')
     # Modify Table
     elif choice == 3:
         showTables()
@@ -202,8 +345,18 @@ while True:
             elif mod_choice == 2:
                 dropLoot()
                 os.system('cls')
-            # elif mod_choice == 3:
-                # Modify Entries
+            elif mod_choice == 3:
+                modOption()
+                mod_option = int(input())
+                if mod_option == 1:
+                    printLoot()
+                    key = int(input("Enter id: "))
+                    pos = input("Enter field: ")
+                    updateLoot(pos, key)
+                elif mod_option == 2:
+                    printLoot()
+                    key = int(input("Enter id: "))
+                    deleteLootData(key)
         elif pop_choice.upper() == "ESTABLISHMENT":
             if mod_choice == 1:
                 popEst()
@@ -211,8 +364,18 @@ while True:
             elif mod_choice == 2:
                 dropEst()
                 os.system('cls')
-            # elif mod_choice == 3:
-                # Modify Entries
+            elif mod_choice == 3:
+                modOption()
+                mod_option = int(input())
+                if mod_option == 1:
+                    printEst()
+                    key = int(input("Enter id: "))
+                    pos = input("Enter field: ")
+                    updateEst(pos, key)
+                elif mod_option == 2:
+                    printEst()
+                    key = int(input("Enter id: "))
+                    deleteEstData(key)
         elif pop_choice.upper() == "ENCOUNTER":
             if mod_choice == 1:
                 popEnc()
@@ -220,8 +383,18 @@ while True:
             elif mod_choice == 2:
                 dropEnc()
                 os.system('cls')
-            # elif mod_choice == 3:
-                # Modify Entries
+            elif mod_choice == 3:
+                modOption()
+                mod_option = int(input())
+                if mod_option == 1:
+                    printEnc()
+                    key = int(input("Enter id: "))
+                    pos = input("Enter field: ")
+                    updateEnc(pos, key)
+                elif mod_option == 2:
+                    printEnc()
+                    key = int(input("Enter id: "))
+                    deleteEncData(key)
         # After choosing a table, you can choose to Update, Drop or Generally modify the Table
     # Create Table
     elif choice == 4:
@@ -238,7 +411,7 @@ while True:
         elif table_choice == 3:
             createEncTable()
             os.system('cls')
-    #EXIT PROGRAM
+    # EXIT PROGRAM
     elif choice == 5:
         print("EXITING PROGRAM...")
         exit()
